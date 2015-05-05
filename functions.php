7cffa6626fd302f1be557eec3f05eadbaffc38c3
Simon <pross@pross.org.uk>
2015-05-05 22:58:53 +0100

    enqueue that

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+
+class PageLines_Ten {
+
+  function __construct() {
+    add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+  }
+
+  // enqueue styles and scripts.
+  function scripts() {
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+  }
+}
+
+new PageLines_Ten;
