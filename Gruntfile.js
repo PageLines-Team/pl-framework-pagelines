162838a7ff393aaac4651dc727349bf662a4505b
Andrew Powers <andrew@pagelines.com>
2015-10-06 09:55:49 -0600

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,87 @@
+/**
+ * Boilerplate Gruntfile.js
+ */
 module.exports = function(grunt) {
 
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
+
+    /**
+     * Setup some basic variables to use later
+     */
     var pkg = grunt.file.readJSON('package.json');
-		var slug = process.cwd().substr(process.cwd().lastIndexOf('/') + 1);
+    var slug = process.cwd().substr(process.cwd().lastIndexOf('/') + 1);
+    var remote = 'git@github.com:PageLines-Team/' + slug + '.git'
+    var build_msg = grunt.option('message') || ''
 
+    /**
+     * Main Grunt config start
+     */
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      clean: pkg.cleanFolders,
-      copy: {
-        main: {
-          files: [
-            // includes files within path
-            {
-              expand: true,
-              src: [ '**', pkg.copyIgnores ],
-              dest: 'src/' + slug + '/',
-              filter: 'isFile'
-            },
-          ],
-        }
-      },
-      compress: {
-        main: {
+
+      // clean task, an array of folders to clean
+      clean: ['dist','src'],
+      // watch task, in this case we just watch one file build.less and when it changes run less compiler
+      watch: {
+        lessMain: {
+          // what files/folder we watching?
+          files: [ 'build.less' ],
+          // tasks to run in order when something changes
+          tasks: ['less:compileMain'],
           options: {
-            mode: 'zip',
-            archive: 'dist/' + slug + '.zip'
-          },
-          expand: true,
-          cwd: 'src/',
-          src: ['**']
+              nospawn: true,
+          }
+        }
+        },
+      // The LESS task, compiles build.less into a usable style.css file
+      less: {
+        compileMain: {
+          src: 'build.less',
+          dest: 'style.css',
+          options: {
+            strictMath: true,
+            sourceMap: false                }
+          }
+      },
+      // copy task, copy all files into a dist folder.
+      // uses pkg.copyIgnores variable which is an array of files and folders to ignore.
+      // They are set in package.json folder
+      copy: {
+        build: {
+          files: [ {
+            expand: true,
+            src: [ '**', pkg.copyIgnores ],
+            dest: 'dist/',
+            filter: 'isFile'
+          } ]
         }
       },
-		    cssmin: {
-		      options: {
-		        shorthandCompacting: false,
-		        roundingPrecision: -1,
-						keepSpecialComments: 1
-		      },
-		      main: {
-		        src: 'style.css',
-		        dest: 'style.css'
-		      }
-		    },
-		    concurrent: {
-		        all: {
-		          //  tasks: ['watch:js', 'watch:less', 'watch:php'],
-		            tasks: [ 'watch:lessMain', 'watch:lessSections'],
-		            options: {
-		                logConcurrentOutput: true
-		            }
-		        }
-		    },
-		    less: {
-
-			    compileMain: {
-	            src: 'build.less',
-              dest: 'style.css',
-                options: {
-                    strictMath: true,
-                    sourceMap: false                
-                }
-	        
-          }, 
-          compileSections: {
-	                src:    'sections/*/build.less', 
-	                dest:   '',
-	                expand: true,
-	                rename: function(dest, src) {
-	                    return src.replace("build.less", "style.css");
-	                },
-
-	         }
-		    },
-        watch: {
-
-            /** Watch Sections' build.less files */
-            lessSections: {
-                files: [ 'sections/*/build.less' ], // files to watch
-                tasks: [ 'less:compileSections' ],  // what to do on change
-                options: { nospawn: true }          // No child process - 500ms faster
-            },
-            lessMain: {
-                // what files/folder we watching?
-                files: [ 'build.less' ],
-                // tasks to run in order when something changes
-                tasks: ['less:compileMain'],
-                options: {
-                    nospawn: true,
-                }
-            }
+      // build task, pushes contents of dist folder to remote 'build' branch.
+      buildcontrol: {
+        options: {
+          dir: 'dist',
+          commit: true,
+          push: true,
+          message: build_msg + ' [Built from commit "%sourceCommit%"]'
+        },
+        production: {
+          options: {
+            remote: remote,
+            branch: 'build'
           }
-
-
+        },
+        staging: {
+          options: {
+            remote: remote,
+            branch: 'staging'
+          }
+        }
+      }
     });
-
-    /** Run on GRUNT initialization */
-    grunt.registerTask( 'default', 	[ 'clean', 'less', 'concurrent' ] );
-
-    grunt.registerTask('release', [
-      'clean',          // clean the folders
-			'less',						// build that less
-      'copy',           // copy the files we need
-      'compress'       // create out zip
-    ]);
+    // Default grunt task,
+    grunt.registerTask( 'default', [ 'clean', 'less', 'watch'] );
+    // grunt build task for production
+    grunt.registerTask( 'build', [ 'clean', 'less', 'copy:build', 'buildcontrol:production'] );
+    // grunt build task for staging
+    grunt.registerTask( 'stage', [ 'clean', 'less', 'copy:build', 'buildcontrol:staging'] );
 }
