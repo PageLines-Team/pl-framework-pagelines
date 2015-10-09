28280fe8278287c832e180edbe74ca20e80918cc
Simon Prosser <simon@pagelines.com>
2015-10-09 21:14:16 +0100

    [Built from commit "0120011"]

---
+++ page-home.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Template Name: Home Page
+ * Description: The PageLines.com homepage.
+ */
+
+
+
+$splashup_settings = array(
+
+    'header'                => 'Platform 5', 
+    'subheader'             => 'Insanely-simple design, editing and layout for WordPress.<br/> Coming November 2015.', 
+    'logo'                  => get_stylesheet_directory_uri() . '/images/pl-logo.png', 
+    'logo_height'           => '35', 
+    'footer_text'           => '&copy; PageLines Inc. 2015', 
+    'button_primary'        => 'http://www.pagelines.com/my-account/', 
+    'button_primary_text'   => 'Signup For Early Access &nbsp; <i class="pl-icon pl-icon-caret-right"></i>', 
+    'button_primary_style'  => 'primary', 
+
+  ); 
+
+echo pl_get_section( array( 'section' => 'splashup', 'id' => 'homesplash_321', 'settings' => $splashup_settings ) );
+
+
