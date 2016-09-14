3950b957976c42e313af722e3643f0d12ce6f650
Simon Prosser <pross@pross.org.uk>
2016-09-14 01:07:11 +0100

    [Built from commit "5b48afc"]

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 /**
  * Welcome to PageLines.com child theme.
- *  
- * Here are some notes: 
- * 
+ *
+ * Here are some notes:
+ *
  *   Code
  *   PHP Escaping: uses the PHP Nowdoc syntax to help with formatting
  *   https://php.net/manual/en/language.types.string.php#language.types.string.syntax.nowdoc
@@ -13,7 +13,7 @@
  *   Uses the popular Prism JS library
  *   http://prismjs.com/index.html
  *
- * 
+ *
  */
 
 
@@ -28,12 +28,12 @@ function pl_add_syntax_highlighting(){
 ?>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/_plugins/prism/prism.css">
   <script src="<?php echo get_stylesheet_directory_uri();?>/_plugins/prism/prism.js"></script>
-<?php 
+<?php
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_scripts' );
 function enqueue_child_scripts() {
-    
+
   pl_script( 'stickykit', get_stylesheet_directory_uri() . '/_plugins/stickykit/stickykit.js', array('jquery'), false, true );
 
   pl_script( 'pl-pagelines-theme', get_stylesheet_directory_uri() . '/pagelines.js', array('jquery'), false, true );
@@ -51,7 +51,7 @@ function pl_create_code( $code, $lang = 'php' ){
 
   $encoded = htmlentities( $replace_codes );
 
-  $output = sprintf('<div class="code-block" data-bind="stopBinding: true"><pre><code class="language-%s">%s</code></pre></div>', $lang, $encoded); 
+  $output = sprintf('<div class="code-block" data-bind="stopBinding: true"><pre><code class="language-%s">%s</code></pre></div>', $lang, $encoded);
 
   return $output;
 
@@ -62,7 +62,7 @@ function pl_create_code( $code, $lang = 'php' ){
  * EXAMPLE
  * Callback Example. Must be in functions.php since it runs in AJAX
  */
-add_filter('pl_binding_taxlist', 'pl_list_tax', 10, 2);  
+add_filter('pl_binding_taxlist', 'pl_list_tax', 10, 2);
 function pl_list_tax( $response, $data ){
 
   // Value of option / variable assigned to plcallback
@@ -82,9 +82,9 @@ function pl_list_tax( $response, $data ){
     }
   }
 
-  else 
+  else
     return 'No terms returned.';
-  
+
 
   // Return
   $response['template'] = $html;
@@ -98,7 +98,7 @@ function pl_register_menu_locations() {
 }
 
 
-function create_docs_template( $content ){  
+function create_docs_template( $content ){
 
   global $post;
   ?>
@@ -108,16 +108,16 @@ function create_docs_template( $content ){
   <div class="doclist-container">
 
     <div class="doclist pl-row">
-    
+
       <sidebar class="doclist-sidebar pl-col-sm-3">
         <h3>Chapters</h3>
 
-        <?php 
+        <?php
           if( has_nav_menu( 'developer' ) ) {
             echo wp_nav_menu( array( 'theme_location' => 'developer' ) );
           }
           else{
-            printf( '<ul>%s</ul>',  wp_list_pages( array( 'child_of' => wp_get_post_parent_id( $post->ID ), 'title_li' => '', 'echo' => 0 ) ) ); 
+            printf( '<ul>%s</ul>',  wp_list_pages( array( 'child_of' => wp_get_post_parent_id( $post->ID ), 'title_li' => '', 'echo' => 0 ) ) );
           }
           ?>
 
@@ -126,25 +126,109 @@ function create_docs_template( $content ){
 
       </sidebar>
 
-      <div class="doclist-content pl-col-sm-9">    
-        
+      <div class="doclist-content pl-col-sm-9">
+
         <div class="doclist-content-pad docnav-scan">
 
           <h1><?php echo get_the_title();?></h1>
-        
+
           <?php echo $content;?>
 
         </div>
 
-      </div> 
+      </div>
 
-    </div> 
+    </div>
 
-  </div> 
+  </div>
 
 </div>
 
 
-  <?php 
+  <?php
 
+}
+add_shortcode( 'pl_fue_unsubscribe', 'pl_fue_unsubscribe' );
+function pl_fue_unsubscribe() {
+  $order_id       = 0;
+$order_url      = '';
+$email_address  = '';
+$queue_id       = '';
+  ob_start();
+
+  ?>
+  <article class="page type-page status-publish hentry">
+            <header class="entry-header">
+                <h1 class="entry-title"><?php _e('Unsubscribe from Email List', 'follow_up_emails'); ?></h1>
+            </header>
+            <?php
+            $email = '';
+            if (isset($_GET['fue']) && !empty($_GET['fue'])) {
+                $email = $_GET['fue'];
+            } else {
+                $email = $email_address;
+            }
+
+            $eid = isset($_GET['fueid']) ? $_GET['fueid'] : '';
+
+            if ( !empty( $_REQUEST['error'] ) ):
+            ?>
+            <div class="error woocommerce-error">
+                <?php echo wp_kses_post( urldecode( $_REQUEST['error'] ) ); ?>
+            </div>
+            <?php
+            endif;
+            ?>
+            <div class="fue-unsubscribe-form entry-content">
+                <form action="" method="post">
+                    <input type="hidden" name="fue_action" value="fue_unsubscribe" />
+                    <input type="hidden" name="fue_eid" value="<?php echo esc_attr($eid); ?>" />
+                    <input type="hidden" name="fue_qid" value="<?php echo esc_attr($queue_id); ?>" />
+                    <p>
+                        <label for="fue_email"><?php _e('Email Address:', 'follow_up_emails'); ?></label>
+                        <br/>
+                        <input type="text" id="fue_email" name="fue_email" value="<?php echo esc_attr($email); ?>" size="25" />
+                    </p>
+
+                    <?php if ( $order_id ): ?>
+                    <p>
+                        <label for="fue_unsubscribe_order">
+                            <input type="checkbox" name="unsubscribe_order_id" id="fue_unsubscribe_order" value="<?php echo esc_attr( $order_id ); ?>" />
+                            <?php printf( __('Do not send me emails regarding <a href="%s">Order %s</a> again.', 'follow_up_emails'), esc_url( $order_url ), $order->get_order_number() ); ?>
+                        </label>
+                    </p>
+                    <?php endif; ?>
+
+                    <p>
+                        <label for="fue_unsubscribe_all">
+                            <input type="checkbox" name="unsubscribe_all" id="fue_unsubscribe_all" value="yes" />
+                            <?php _e('Do not send me non-order emails again', 'follow_up_emails'); ?>
+                        </label>
+                    </p>
+
+                    <?php do_action('fue_unsubscribe_form', $email); ?>
+                    <p>
+                        <input type="submit" name="fue_submit" id="fue_submit" value="<?php _e('Unsubscribe', 'follow_up_emails'); ?>" />
+                    </p>
+                </form>
+            </div>
+        </article>
+        <script>
+        jQuery(document).ready(function($) {
+
+            $(".fue-unsubscribe-form :input[type=checkbox]").change(function() {
+                var num_checked = $(".fue-unsubscribe-form :input[type=checkbox]:checked").length;
+
+                if ( num_checked > 0 ) {
+                    $("#fue_submit").attr("disabled", false);
+                } else {
+                    $("#fue_submit").attr("disabled", true);
+                }
+            }).change();
+
+        } );
+        </script>
+        <?php
+
+  return ob_get_clean();
 }
